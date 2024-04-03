815ac72ec187f2df00fd733678b39e6199c5d0d0
fludy24 <102822920+fludy24@users.noreply.github.com>
2024-04-03 13:03:58 +0300

    Add files via upload

---
+++ лаба 2 і.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <windows.h>
+int count_multiples(int n)
+{
+	int count_2 = n / 2;
+	int count_3 = n / 3;
+	int count_6 = n / 6;
+	return count_2 + count_3 - count_6;
+}
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int n;
+	std::cout << "Введіть n: ";
+	std::cin >> n;
+	int count = count_multiples(n);
+	std::cout << "Кількість кратних 2 і 3: " << count << std::endl;
+	return 0;
+}
